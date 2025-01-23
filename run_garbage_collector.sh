e60aba87b77c64de527c0fbd7150641f34db4488
James Billard <james.billard@avalara.com>
2025-01-23 10:43:27 -0800

    Housekeeping

--- run_garbage_collector.sh
+++ run_garbage_collector.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Activate the virtual environment if required
-source .venv/bin/activate
+source venv/bin/activate
 
+# Install dependencies from requirements.txt
+pip install -r requirements.txt
+
+playwright install
 # Run the Python script
 python3 main.py
