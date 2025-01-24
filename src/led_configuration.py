d104e485f8d18c434d6db178e75d1fb96556beab
James Billard <james.billard@avalara.com>
2025-01-24 10:50:04 -0800

    Added back in pulsate white.

--- src/led_configuration.py
+++ src/led_configuration.py
@@ -164,6 +164,13 @@ def fade_to_color(collections, BASE_COLOR, steps=100, interval=0.02, hold_time=5
             if not params or "fade_state" not in params:
                 return
 
+            fade_state = params.fade_state
+            collections = fade_state.collections
+            BASE_COLOR = fade_state.base_color
+            steps = fade_state.steps
+            interval = fade_state.interval
+            hold_time =5
+
             """
                 Start at the base color, fade each group to its collection color, then fade all back to the base color.
                 """
